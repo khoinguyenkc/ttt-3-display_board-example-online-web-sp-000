98fe8ab01d4d2f45577eb8fc92534ba41515a1c7
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-10 03:03:56 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,12 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-def display_board
-  puts"   |   |    "
+def display_board(position)
+  myarray = [" "," "," "," "," "," "," "," "," ",]
+  myarray[position-1] = "X"
+  puts"   |   |   "
   puts"-----------"
-  puts"   |   |    "
+  puts"   |   |   "
   puts"-----------"
-  puts"   |   |    "
+  puts"   |   |   "
 end
 
 display_board
